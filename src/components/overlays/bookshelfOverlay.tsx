91439e2acee635922fb5183c474984b6b7eeef7a
Juan Francisco Romero Fernández <jfranromfdez@gmail.com>
2026-03-12 19:35:00 +0100

    feat: book fixed

--- src/components/overlays/bookshelfOverlay.tsx
+++ src/components/overlays/bookshelfOverlay.tsx
@@ -8,12 +8,10 @@ import BookIndex from "./bookComponents/bookIndex";
 
 export default function BookshelfOverlay() {
   const [page, setPage] = useState(0);
-  const [direction, setDirection] = useState("next");
 
+  // Si la página actual es mayor que el índice, aplicamos la clase "next"
   const getPageClass = (index) => {
-    if (direction === "next" && page > index) return "next";
-    if (direction === "back" && page <= index) return "back";
-    return "";
+    return page > index ? "next" : "";
   };
 
   return (
@@ -23,24 +21,9 @@ export default function BookshelfOverlay() {
       </p>
 
       <button
-        className={`pageBtn ${page > 0 ? "" : "disabled"}`}
-        onClick={() => {
-          if (page > 0) {
-            setDirection("back");
-            setPage(page - 1);
-          }
-        }}
-      >
-        Volver Página
-      </button>
-
-      <button
         className={`pageBtn nextPage ${page < 4 ? "" : "disabled"}`}
         onClick={() => {
-          if (page < 4) {
-            setDirection("next");
-            setPage(page + 1);
-          }
+          if (page < 4) setPage(page + 1);
         }}
       >
         Pasar Página
